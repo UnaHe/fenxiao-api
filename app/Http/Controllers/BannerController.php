407d61ef5c282944df532219be20f3cf0bdfe683
formatcc <young123tao@163.com>
2018-03-06 11:40:43 +0800

    'banner增加模板变量'

--- app/Http/Controllers/BannerController.php
+++ app/Http/Controllers/BannerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Services\BannerService;
+use App\Services\UserService;
 use Illuminate\Http\Request;
 use App\Services\TaobaoService;
 
@@ -16,11 +17,12 @@ class BannerController extends Controller
      * 获取指定位置广告
      */
     public function getBanner(Request $request, $position){
+        $userId = $request->user()->id;
         if(!$position){
             return $this->ajaxError("参数错误");
         }
 
-        $data = (new BannerService())->getBanner($position);
+        $data = (new BannerService())->getBanner($userId, $position);
         return $this->ajaxSuccess($data);
     }
 
--- app/Services/BannerService.php
+++ app/Services/BannerService.php
@@ -16,14 +16,32 @@ class BannerService
      * 获取指定位置广告
      * @return mixed
      */
-    public function getBanner($position){
+    public function getBanner($userId, $position){
         if($cache = CacheHelper::getCache()){
             return $cache;
         }
 
         $data = Banner::select('name', 'pic', 'click_url')->where(['position'=>$position, 'is_delete'=>0])->get();
+
+        $pid = "mm_99303416_22298718_74102081";
+        if($userId){
+            $pidInfo = (new UserService())->getPidInfo($userId);
+            if($pidInfo){
+                $pid = $pidInfo['pid'];
+            }
+        }
+        $templateData = [];
+        $templateData['user_id'] = $userId;
+        $templateData['pid'] = $pid;
+
+        foreach ($data as &$item){
+            foreach ($templateData as $name=>$value){
+                $item['click_url'] = str_replace('{'.$name.'}', $value, $item['click_url']);
+            }
+        }
+
         if($data){
-            CacheHelper::setCache($data, 5);
+            CacheHelper::setCache($data, 2);
         }
         return $data;
     }
